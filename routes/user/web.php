4182efd9c1a32f2de2e616771183a80b391e49a7
waiphyohlaing <waiphyohlaing@securelinkmm.com>
2023-03-06 01:19:04 +0630

    First commit

---
+++ routes/user/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+ | user route
+ | routes are loaded by the RouteServiceProvider
+*/
+
+// Route::get('/list', 'User\NewController@index')->name('users.list');
+Route::get('/dd', function () {
+    return 'hee hee';
+});
